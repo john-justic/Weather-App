27f6cdfbfda424ffc67f7d3be3409cf9db9b51e1
John <johnjustice766@gmail.com>
2022-12-18 16:40:16 +0300

    Done

--- index.js
+++ index.js
@@ -118,26 +118,26 @@ function forecast(cityid){
             $("#fTemp"+i).html(tempF+"&#8457");
             $("#fHumidity"+i).html(humidity+"%");
         }
-        
+
     });
 }
 
-// //Daynamically add the passed city on the search history
-// function addToList(c){
-//     var listEl= $("<li>"+c.toUpperCase()+"</li>");
-//     $(listEl).attr("class","list-group-item");
-//     $(listEl).attr("data-value",c.toUpperCase());
-//     $(".list-group").append(listEl);
-// }
-// // display the past search again when the list group item is clicked in search history
-// function invokePastSearch(event){
-//     var liEl=event.target;
-//     if (event.target.matches("li")){
-//         city=liEl.textContent.trim();
-//         currentWeather(city);
-//     }
+//Daynamically add the passed city on the search history
+function addToList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+// display the past search again when the list group item is clicked in search history
+function invokePastSearch(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
+    }
 
-// }
+}
 
 // // render function
 // function loadlastCity(){
